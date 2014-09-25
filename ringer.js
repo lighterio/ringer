a1964a169c8a36ce1dbc56665242ed9ea2002363
Sam Eubank <sameubank+zerious@gmail.com>
2014-09-24 19:19:53 -0700

    README.md and module beginnings

--- ringer.js
+++ ringer.js
@@ -1,7 +1,35 @@
-var api = module.exports;
+var cluster = require('cluster');
+var os = require('os');
+
+var Ring = require('./lib/ring');
 
 /**
- * Expose the version to module users.
- * @type {string}
+ * Start a Ringer process on each core.
  */
-api.version = require('./package.json').version;
+var ringer = module.exports = function (options) {
+  options = options || {};
+  options.httpPort = options.httpPort || process.env.PORT || 8888;
+  options.firstPort = options.firstPort || process.env.FIRST_PORT || 12300;
+  if (!options.server) {
+    try {
+      options.server = require('za')();
+      options.server.listen(options.httpPort);
+    }
+    catch (e) {
+    }
+  }
+  if (cluster.isMaster) {
+    os.cpus().forEach(cluster.fork);
+    options.server.close();
+  }
+  else {
+    new Ring(options);
+  }
+};
+
+// Expose the version number, but only load package JSON if it's requested.
+Object.defineProperty(ringer, 'version', {
+  get: function () {
+    return require('./package.json').version;
+  }
+});
